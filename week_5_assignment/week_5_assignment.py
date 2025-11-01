5aad920d868454a45e0a4b6d1bb2d6142d08cd36
Oktabrovs <oktabrovumrbek2023@gmail.com>
2025-11-01 17:23:14 +0500

    week5-v2

--- week_5_assignment/week_5_assignment.py
+++ week_5_assignment/week_5_assignment.py
@@ -3,6 +3,7 @@ def find_account_index(account_ids, account_id):
         if account_ids[i] == account_id:
             return i
     return -1
+    
 def process_ledger(initial_accounts, initial_balances, transactions):
     c_accounts = initial_accounts[:]
     c_balance = initial_balances[:]
@@ -22,18 +23,3 @@ def process_ledger(initial_accounts, initial_balances, transactions):
     final_account_ids_list = c_accounts
     final_account_balances_list = c_balance
     return final_account_ids_list, final_account_balances_list
-
-accounts = ["ACC-001", "ACC-002", "ACC-003"]
-balances = [500.00, 1200.00, 250.00]
-daily_transactions = [
-    ["WITHDRAW", "ACC-001", 500],
-    ["WITHDRAW", "ACC-002", 250.00],
-    ["WITHDRAW", "ACC-003", 300.00], # This should fail (insufficient funds)
-    ["OPEN", "ACC-004", 1000.00],
-    ["DEPOSIT", "ACC-002", 50.00],
-    ["DEPOSIT", "ACC-001", 2000],
-    ["OPEN", "ACC-005", 1500000000]
-]
-final_accounts, final_balances = process_ledger(accounts, balances, daily_transactions)
-print(f'''Final Accounts: {final_accounts}
-Final Balances: {final_balances}''')
